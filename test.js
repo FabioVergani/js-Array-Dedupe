0a73f4e5933d365dac5ee7346447a9672f361b63
Fabio Vergani <fabio.vergani.1975@gmail.com>
2019-11-03 18:59:39 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,4 @@
-console.clear();
-//
-let test;
-//test = [1, 33, 2, 3, 4, 5, 6, 7, , 8, , 9, 0, 1, 2, 2, 2, 2, 1, 'a', 'a', 'b'];
-test = [1, 5, , 2, 3, 4, 4, , 5, 5, 6, ];
-//
-//test = [,undefined,undefined,2];
-//console.log(test.length,test[3])
-//test = [];
-
-//=========================
-
-const dedupeDisruptive=a=>{
+const dedupeArrayDisruptive=a=>{
   for(const v of a){
     const i=a.indexOf(v);
     let j=a.lastIndexOf(v);
@@ -22,7 +10,7 @@ const dedupeDisruptive=a=>{
   return a
 };
 
-const dedupeConservative=a=>{
+const dedupeArrayConservative=a=>{
   const m=[];
   let j=0;
   for(const v of a){
@@ -36,24 +24,71 @@ const dedupeConservative=a=>{
   return m
 };
 
-//====================================================================
-console.log(test,dedupeConservative(test));
+const condenseArrayDisruptive=a=>{
+  const u=undefined;
+  let i=a.length;
+  while(i){
+    if(u===a[i]){
+      a.splice(i,1)
+    };
+    --i
+  };
+  return a
+};
+
+const condenseArrayConservative=a=>{
+  const u=undefined,m=[],l=a.length;
+  let i=0,j=0;
+  while(i<l){
+    const v=a[i];
+    if(u!==v){
+      m[j]=v
+      ++j
+    };
+    ++i
+  };
+  return m
+};
+
+//===============================================================================
+console.clear();
+let test= [1,, 5, , 2, 3, 4, 4, , 5, 5, 6, ];
+//
+console.log(test,dedupeArrayConservative(test));
 /*
-§
+
   [1,33,2,3,4,5,6,7,undefined,8,undefined,9,0,1,2,2,2,2,1,"a","a","b"]
   [1,33,2,3,4,5,6,7,undefined,8,undefined,9,0,"a","b"]
-§
+
   [1,5,undefined,2,3,4,4,undefined,5,5,6]
   [1,5,undefined,2,3,4,undefined,6]
+  
 */
-console.log(test,dedupeDisruptive(test));
+
+console.log(test,dedupeArrayDisruptive(test));
 /*
-§
+
   [1,33,2,3,4,5,6,7,undefined,8,undefined,9,0,"a","b"]
   [1,33,2,3,4,5,6,7,undefined,8,undefined,9,0,"a","b"]
-§
+
   [1,5,undefined,2,3,4,undefined,6]
   [1,5,undefined,2,3,4,undefined,6]
+
 */
 
 
+console.log(test,condenseArrayConservative(test));
+/*
+
+  [1,undefined,5,undefined,2,3,4,undefined,6]
+  [1,5,2,3,4,6]
+
+*/
+
+console.log(test,condenseArrayDisruptive(test));
+/*
+
+  [1,5,2,3,4,6]
+  [1,5,2,3,4,6]
+
+*/
